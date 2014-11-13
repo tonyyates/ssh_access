913c633563ac892cd85c1e3480c22d1ba0d91609
Tony Yates <tonyyates@gmail.com>
2014-11-13 10:37:36 +0000

    modified key in userfile and typo in tony file

--- create_tony_user_with_sudo_access.sh
+++ create_tony_user_with_sudo_access.sh
@@ -8,7 +8,7 @@ tony     ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
 chmod 0440 /etc/sudoers.d/tony-ALL
 mkdir ~tony/.ssh
-chmod 700 tonypeter/.ssh
+chmod 700 tony/.ssh
 curl https://raw.githubusercontent.com/tonyyates/ssh_access/master/tonyyates.pub >~tony/.ssh/authorized_keys
 chmod 400 ~tony/.ssh/authorized_keys
 chown -R tony ~tony/.ssh
--- create_user_with_sudo_access.sh
+++ create_user_with_sudo_access.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 MY_SHELL=$( which bash )
-new_user=jayk
+new_user=tony
 
-/usr/sbin/useradd -c 'Jay Kola <jay at noesisinformatica.com>' -s ${MY_SHELL} -m ${new_user}
+/usr/sbin/useradd -c 'Tony Yates <tonyyates@gmail.com>' -s ${MY_SHELL} -m ${new_user}
 cat >/etc/sudoers.d/${new_user}-ALL <<EOF
 ${new_user}     ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
 chmod 0440 /etc/sudoers.d/${new_user}-ALL
 mkdir /home/${new_user}/.ssh
 chmod 700 /home/${new_user}/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDjb0RsR8ZtABGIRC/uzjZZClq2drvH58SiLgDQhU02+U3V1AKTAIk/SorCCYBgsFdypjhOoOODH8fzkCURzUqJIYwNEmGd9agQcT4IWiHux5voJxLH9lUstK4ZFZgglkwK0/40lUxq7jxJLcw+m1eMxfnp7mGel5eR5qi7lHY6DBIi2MkLPO3j5EgwCA6YI7xuMgYBqhOgigzZ0Yg89l9Pf6VwV+BAnUXM7Po9AKXe82k4y2faLHoBDfA8OUdqoZYiPgEE8diFt/fRc+7ZPFK9PrHX9pxTnrCW9XM0/cKG4ZM9ONvYKtoE/dL4mQEkVNNVd6K4Igo9ee6dGSW0yalixvlyn7nvp3e77x1Dkr1oXTIzASxEfJ4QUj8sUdv/aaE5q/h1rK+lih6FpkHNpFd9nVaV+byWiBPT9pEey0ukGc0hj7ET9BnZSAexIMXBI4eU+RsMwl+E8ls/xqdwIikhSNQeohSX2pNjnrxBFG9xxp9Ykh4KC2I1nJWDUZyj8Y0OvP7A63QL7hdKqlhdXsGvaDqJKiSaOJm5uNB5zDm3KYgslCuHiKoP1lJJ33xgWxAMSqIF4oLMX5vlB0l7JcYWmvr1idj9cfOh1C9P9rrtjsifr+3nhT03gtqi/zD2oR3wdn1LJOi/o5pc9KeKXEdgu3sQAii58SP3/NwMLsC4CQ== jay@Cerberus" >/home/${new_user}/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuZ7Dw54cLxLH2tvZLqKfCmZfzgkK5RgR/AZkNNEiyBsY27GpR1DWLbG+RfZfFMR2nEd1qRc3REQgPHK4PSJazx34md+lXOXK/NKkc2jmr0c6nhFAdjzKZXlEjk3nFmHtnkbfmrvSXVEUkvfHpYr/Pka0qCXUCNmR8qCc9UiMcEph6LLvvXc6/zohchLCjpz0gDTyS7/VWKcipBOdTTSddenHP6cWoCBtPWhu2u0+4M6myk0CZaH/fZ4nIxKLFY9P7EHLXFcO1QMH5owLFaoxrvNBonR7lV1zIsZAfZ7D26hFyUFNb8VjwCPIH2tEEijtEwx2ljx/dTkeK90EG+/2V tony@Tonys-15-MacBook-Pro.local" >/home/${new_user}/.ssh/authorized_keys
 chmod 400 /home/${new_user}/.ssh/authorized_keys
 chown -R ${new_user} /home/${new_user}/.ssh
